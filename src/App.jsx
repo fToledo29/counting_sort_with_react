4446f4ba2a7ff02279c6ed6e22e648e8d1a00521
Fernando Toledo <fernando_29837@live.com>
2021-04-14 04:39:06 +0000

    Implementing Bucket Sort

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,24 @@
 import React, { useEffect, useState } from 'react';
 import { Button } from '@material-ui/core';
+import Select from '@material-ui/core/Select';
 import Tablext from './Tablext/Tablext';
 import API from './api/addressApi';
 import Helper from './utils/helper';
 import './App.css';
+import { ALGORITHM_NAMES, ALGORITHM_NAMES_ARR } from './shared/constants';
 
 function App() {
 
-	let [data, changeData] = useState([]);
+	const ADDRESS_DATA = 'addressData';
+	const ALGO_NAME = 'algoName';
+
+	let [data, changeData] = useState({[ADDRESS_DATA]: [], [ALGO_NAME]: ''});
+	let [algoName, changeAlgo] = useState('Select');
+
 
 	const getMoreData = () => {
 		API.getData().then((datax) => {
-			changeData(datax.map(x => {
+			const newData = datax.map(x => {
 				return {
 					street_address_num: x.street_address.split(' ')[0],
 					street_address: x.street_address,
@@ -19,46 +26,79 @@ function App() {
 					zip: x.zip,
 					street_name: x.street_name
 				};
-			}));
+			});
+
+			updateStateProperty(newData, '');
+
 		}).catch(error => {
 			console.log('Something happened: ', error);
 		})
 	};
+
+	const updateStateProperty = (newData, algoName) => {
+		const dataCopy = {...data};
+		dataCopy[ADDRESS_DATA] = newData;
+		dataCopy[ALGO_NAME] = algoName;
+		changeData({...data, ...dataCopy});
+	}
 	
 	const sortData = () => {
-		const sortedObj = Helper.sort(data, 'id');
-		changeData(sortedObj.data);
-		consoe.log('algorithm: ', sortedObj.algorithm)
+		const sortedObj = Helper.sort(data.addressData, 'id', algoName);
+		updateStateProperty(sortedObj.data, sortedObj.algorithm);
 	}
 
 	useEffect(() => {
 		getMoreData();
 	}, []);
 
+	const onSelectChange = () => {
 
+	}
 
 	return (
 			<>
 				<div className="main-body"> 
 					<Button
-					className="btn-refresh"
+					className="btn-refresh elm-center"
 					onClick={() => getMoreData()} 
 					variant="contained" 
 					color="primary">
 						Refresh Data
 					</Button>
 					<Button
-					className="btn-sort"
+					disabled={algoName === 'Select'}
+					className="btn-sort elm-center"
 					onClick={() => sortData()}
 					variant="contained" 
 					color="secondary">
 						Sort Data
 					</Button>
+
+					 <Select
+					 className="elm-center"
+					native
+					value={algoName}
+					onChange={(e => changeAlgo(e.target.value))}
+					>
+					<option aria-label="None" value="" />
+					<option value={'Select'}>Select Algorithm</option>
+					{ALGORITHM_NAMES_ARR.map((name, ind) => {
+						return <option key={ind} value={name}>{name}</option>
+					})}
+
+					</Select>
+					
+					<label className="algo-name-lbl elm-center ">
+						Algorithm: <span className="algo-name"> {data.algoName} </span>
+					</label>
+					
 				
-					<Tablext data={data}/>
+					<Tablext data={data.addressData}/>
 				</div>
 			</>
 	);
 }
+
+
 
 export default App;
--- src/utils/helper.js
+++ src/utils/helper.js
@@ -1,11 +1,60 @@
+import { ALGORITHM_NAMES, ALGORITHM_NAMES_ARR } from '../shared/constants';
 
 export default class Helper {
+
+	static bucketSort(inpArr, property, pos) {
+
+		const size = inpArr.length;
+
+		let i = 0;
+ 
+		const range = 10;
+
+		let outputArr = [];
+
+		const maxValue = inpArr.reduce((acc, val, ind) => acc > val[property] ? acc : val[property], 0);
+
+		let bucketArr = Array.from({length: range}, () => []);
+
+		while(i < size) {
+
+			const index = parseInt((inpArr[i][property]  / pos) % 10, 10);
+
+			bucketArr[index].push(inpArr[i]);
+
+			i++;
+		}
+
+		i = 0;
+
+		while(i < bucketArr.length) {
+
+			let j = i;
+
+			while(bucketArr[i].length > 0) {
+
+				outputArr.push(bucketArr[i].shift());
+			
+			}
+
+			i++;
+		}
+
+		pos = parseInt((pos * 10), 10);
+
+		while(parseInt(maxValue / pos, 10) > 0) {
+			outputArr = Helper.bucketSort(outputArr, property, pos);
+			pos = parseInt((pos * 10), 10);
+		}
+
+		return outputArr;
+	}
 
 	static countingSort(arr = [], property) {
 		const n = arr.length;
 
-		const range = arr.reduce((acc, val) => acc > val[property] ? acc : val[property], 0);
-
+		let range = arr.reduce((acc, val) => acc > val[property] ? acc : val[property], 0);
+	
 		const countArr = Array.from({length: range + 1}, () => 0);
 
 		const outputArr = Array.from({length: n}, () => undefined);
@@ -32,10 +81,31 @@ export default class Helper {
 		return outputArr;
 	}
 
-	static sort(data, property) {
-		return {
-			data: Helper.countingSort(data, property),
-			algorithm: Helper.countingSort.name
-		};
+	static sort(data, property, algoName = '') {
+		if (!data.length) {
+			return { 
+				data: [], 
+				algorithm: `Please click "Refresh data"`,
+			};
+		}
+	
+		switch(algoName) {
+			case ALGORITHM_NAMES.COUNTING_SORT:
+				return {
+					data: Helper.countingSort(data, property),
+					algorithm: algoName
+				};
+			case ALGORITHM_NAMES.BUCKET_SORT:
+				return {
+					data: Helper.bucketSort(data, property, 1),
+					algorithm: algoName
+				};
+			default:
+				return { 
+					data: [], 
+					algorithm: 'Algorithm not implemented yet, please refresh and try another one!',
+				};
+		}
+		
 	}
 }
